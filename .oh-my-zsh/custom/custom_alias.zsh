645efc1be58ef6a1a491c572f0f3c26f0454ecdb
Damien Robertson <damien.robertson@shopify.com>
2016-02-21 12:44:57 -0500

    Updated zshrc since custom_aliases was overeaching

--- .oh-my-zsh/custom/custom_alias.zsh
+++ .oh-my-zsh/custom/custom_alias.zsh
@@ -1,38 +1,34 @@
-alias brfull='bubu && brew doctor'
+# alias brfull='bubu && brew doctor'
 
-# Starscream specific environment variable
-alias cluster='PYTHON_ENV=remote_development'
-export WORKON_HOME=$HOME/.virtualenvs
-source $(brew --prefix)/bin/virtualenvwrapper.sh
-export HADOOP_CONF_DIR=~/Developer/starscream/.cache/spark/current/conf/conf.cloudera.yarn
-export HADOOP_USER_NAME="damienrobertson"
-export PATH="/usr/local/sbin:$PATH"
-export NO_AUTOAUTOLINT=true
+# export WORKON_HOME=$HOME/.virtualenvs
+# source $(brew --prefix)/bin/virtualenvwrapper.sh
 
-# Shopify core and React stuff
-export VAGRANT_CWD=~/Developer/vagrant
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+# Stand alone spark setup
+# export PYSPARK_PYTHON=python3
+# export PYSPARK_DRIVER_PYTHON=ipython
 
 # Make fzf use the silver searcher instead of find
-export FZF_DEFAULT_COMMAND='ag -l -g ""'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_DEFAULT_COMMAND='ag -l -g ""'
+# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # HISTORY
-HISTSIZE=65536
-SAVEHIST=65536
+# HISTSIZE=65536
+# SAVEHIST=65536
 
-alias clear=" clear"
-alias pwd=" pwd"
+# Commands below don't appear in history
+# alias clear=" clear"
+# alias pwd=" pwd"
 
-# Python VirtualEnv
-#alias v='workon'
-#alias v.deactivate='deactivate'
-#alias v.mk='mkvirtualenv --no-site-packages'
-#alias v.mk_withsitepackages='mkvirtualenv'
-#alias v.rm='rmvirtualenv'
-#alias v.switch='workon'
-#alias v.add2virtualenv='add2virtualenv'
-#alias v.cdsitepackages='cdsitepackages'
-#alias v.cd='cdvirtualenv'
-#alias v.lssitepackages='lssitepackages'
+## Shopify setup below
+# Starscream specific environment variable
+# alias cluster='PYTHON_ENV=remote_development'
+# export HADOOP_CONF_DIR=~/Developer/starscream/.cache/spark/current/conf/conf.cloudera.yarn
+# export HADOOP_USER_NAME="damienrobertson"
+# export NO_AUTOAUTOLINT=true
+# eval "$(rbenv init -)"
+
+# Shopify core and React stuff
+# export VAGRANT_CWD=~/Developer/vagrant
+# export NVM_DIR=~/.nvm
+# source $(brew --prefix nvm)/nvm.sh
+
