daa01c3f85c5f9b648e9ea760ffd2fb39b8e3836
Damien Robertson <damien.robertson@shopify.com>
2017-01-09 22:41:57 -0500

    Initial commit - window resizing and placement

---
+++ HOME/.hammerspoon/init.lua
@@ -0,0 +1,111 @@
+-- Windowed fullscreen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "f", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed left half
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed right half
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "r", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed left third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "h", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed middle third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "j", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed right third
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "k", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (2 * max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed left two-thirds
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "n", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = 2 * max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Windowed right two-thirds
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = 2 * max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
