90cecc8987eddbc822a81d25fd7e58608d83a9b8
Damien Robertson <robertsondamien@gmail.com>
2017-10-21 10:16:57 -0400

    Rework package and use-package setup

--- HOME/.emacs.d/init.el
+++ HOME/.emacs.d/init.el
@@ -7,23 +7,21 @@
 ;; Package Management
 
 (require 'package)
-
-(setq
- package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-		    ("org" . "http://orgmode.org/elpa/")
-                    ("melpa" . "http://melpa.org/packages/")
-                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
- package-archive-priorities '(("melpa-stable" . 1)))
-
-;; Activate installed packages
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa"           . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable"    . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu"             . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org"             . "http://orgmode.org/elpa/"))
 (package-initialize)
 
-;; Configure use-package
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
